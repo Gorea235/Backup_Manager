e44ab51b9db1642e36c8ea264697f0978529b3d8
Gorea235 <lkdvs3@gmail.com>
2014-02-22 12:38:05 +0000

    Worked on main process, backup loading, etc
    Backup loading now has support for more data, the main process has been
    worked on to form the skeleton of the backup process.

--- Backup_Manager/Form1.Designer.vb
+++ Backup_Manager/Form1.Designer.vb
@@ -42,8 +42,16 @@ Partial Class Main
         Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
         Me.exit_tool = New System.Windows.Forms.ToolStripMenuItem()
         Me.ckbx_startOnBoot = New System.Windows.Forms.CheckBox()
+        Me.nud_previousQuantity = New System.Windows.Forms.NumericUpDown()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.tmr_main = New System.Windows.Forms.Timer(Me.components)
+        Me.nud_minutesPast = New System.Windows.Forms.NumericUpDown()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.btn_startBackup = New System.Windows.Forms.Button()
         CType(Me.nud_interval, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.notifyIcon_menustrip.SuspendLayout()
+        CType(Me.nud_previousQuantity, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.nud_minutesPast, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'lbx_backups
@@ -84,9 +92,9 @@ Partial Class Main
         'Label2
         '
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(175, 85)
+        Me.Label2.Location = New System.Drawing.Point(175, 62)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(455, 21)
+        Me.Label2.Size = New System.Drawing.Size(349, 21)
         Me.Label2.TabIndex = 6
         Me.Label2.Text = "Original Location:"
         '
@@ -94,7 +102,7 @@ Partial Class Main
         '
         Me.tbx_originalLoc.Enabled = False
         Me.tbx_originalLoc.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.tbx_originalLoc.Location = New System.Drawing.Point(178, 109)
+        Me.tbx_originalLoc.Location = New System.Drawing.Point(178, 86)
         Me.tbx_originalLoc.Name = "tbx_originalLoc"
         Me.tbx_originalLoc.Size = New System.Drawing.Size(452, 23)
         Me.tbx_originalLoc.TabIndex = 5
@@ -102,7 +110,7 @@ Partial Class Main
         'Label3
         '
         Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(175, 158)
+        Me.Label3.Location = New System.Drawing.Point(175, 112)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(455, 20)
         Me.Label3.TabIndex = 8
@@ -112,7 +120,7 @@ Partial Class Main
         '
         Me.tbx_bkupLoc.Enabled = False
         Me.tbx_bkupLoc.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.tbx_bkupLoc.Location = New System.Drawing.Point(178, 181)
+        Me.tbx_bkupLoc.Location = New System.Drawing.Point(178, 135)
         Me.tbx_bkupLoc.Name = "tbx_bkupLoc"
         Me.tbx_bkupLoc.Size = New System.Drawing.Size(452, 23)
         Me.tbx_bkupLoc.TabIndex = 7
@@ -120,7 +128,7 @@ Partial Class Main
         'Label4
         '
         Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(175, 234)
+        Me.Label4.Location = New System.Drawing.Point(175, 161)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(455, 20)
         Me.Label4.TabIndex = 9
@@ -139,10 +147,15 @@ Partial Class Main
         'nud_interval
         '
         Me.nud_interval.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.nud_interval.Location = New System.Drawing.Point(178, 257)
+        Me.nud_interval.Location = New System.Drawing.Point(178, 184)
+        Me.nud_interval.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
         Me.nud_interval.Name = "nud_interval"
         Me.nud_interval.Size = New System.Drawing.Size(452, 23)
         Me.nud_interval.TabIndex = 11
+        Me.nud_interval.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'secondThread
+        '
         '
         'notifyIcon_main
         '
@@ -186,11 +199,65 @@ Partial Class Main
         Me.ckbx_startOnBoot.Text = "Start Backup Manager on boot"
         Me.ckbx_startOnBoot.UseVisualStyleBackColor = True
         '
+        'nud_previousQuantity
+        '
+        Me.nud_previousQuantity.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nud_previousQuantity.Location = New System.Drawing.Point(178, 282)
+        Me.nud_previousQuantity.Name = "nud_previousQuantity"
+        Me.nud_previousQuantity.Size = New System.Drawing.Size(452, 23)
+        Me.nud_previousQuantity.TabIndex = 15
+        '
+        'Label5
+        '
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(175, 259)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(455, 20)
+        Me.Label5.TabIndex = 14
+        Me.Label5.Text = "Amount of previous versions to keep:"
+        '
+        'tmr_main
+        '
+        Me.tmr_main.Interval = 60000
+        '
+        'nud_minutesPast
+        '
+        Me.nud_minutesPast.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nud_minutesPast.Location = New System.Drawing.Point(178, 233)
+        Me.nud_minutesPast.Maximum = New Decimal(New Integer() {59, 0, 0, 0})
+        Me.nud_minutesPast.Name = "nud_minutesPast"
+        Me.nud_minutesPast.Size = New System.Drawing.Size(452, 23)
+        Me.nud_minutesPast.TabIndex = 17
+        '
+        'Label6
+        '
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(175, 210)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(455, 20)
+        Me.Label6.TabIndex = 16
+        Me.Label6.Text = "Minutes past the hour to run backup:"
+        '
+        'btn_startBackup
+        '
+        Me.btn_startBackup.Enabled = False
+        Me.btn_startBackup.Location = New System.Drawing.Point(424, 337)
+        Me.btn_startBackup.Name = "btn_startBackup"
+        Me.btn_startBackup.Size = New System.Drawing.Size(100, 70)
+        Me.btn_startBackup.TabIndex = 18
+        Me.btn_startBackup.Text = "Start Backup Now"
+        Me.btn_startBackup.UseVisualStyleBackColor = True
+        '
         'Main
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(642, 419)
+        Me.Controls.Add(Me.btn_startBackup)
+        Me.Controls.Add(Me.nud_minutesPast)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.nud_previousQuantity)
+        Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.ckbx_startOnBoot)
         Me.Controls.Add(Me.nud_interval)
         Me.Controls.Add(Me.btn_saveChanges)
@@ -211,6 +278,8 @@ Partial Class Main
         Me.Text = "Backups"
         CType(Me.nud_interval, System.ComponentModel.ISupportInitialize).EndInit()
         Me.notifyIcon_menustrip.ResumeLayout(False)
+        CType(Me.nud_previousQuantity, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.nud_minutesPast, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -233,5 +302,11 @@ Partial Class Main
     Friend WithEvents startBackup_tool As System.Windows.Forms.ToolStripMenuItem
     Friend WithEvents ToolStripSeparator1 As System.Windows.Forms.ToolStripSeparator
     Friend WithEvents ckbx_startOnBoot As System.Windows.Forms.CheckBox
+    Friend WithEvents nud_previousQuantity As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents tmr_main As System.Windows.Forms.Timer
+    Friend WithEvents nud_minutesPast As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents btn_startBackup As System.Windows.Forms.Button
 
 End Class
--- Backup_Manager/Form1.vb
+++ Backup_Manager/Form1.vb
@@ -1,5 +1,18 @@
 ﻿Imports Microsoft.VisualBasic.FileIO.FileSystem
 Public Class Main
+    Private Class queueClass
+        Public originalLoc As String
+        Public backupLoc As String
+        Public pastVersions As Integer
+
+        Public Sub New(ByVal OriginalLocation As String, ByVal BackupLocation As String, ByVal VersionsToKeep As Integer)
+            Me.originalLoc = OriginalLocation
+            Me.backupLoc = BackupLocation
+            Me.pastVersions = VersionsToKeep
+        End Sub
+    End Class
+    Private queued As Dictionary(Of Integer, queueClass) = New Dictionary(Of Integer, queueClass)
+
     Public Sub New()
 
         ' This call is required by the designer.
@@ -42,11 +55,50 @@ Public Class Main
     End Sub
 
     Private Sub startBackup_tool_Click(sender As Object, e As EventArgs) Handles startBackup_tool.Click
-
+        Dim seleced As BackupClass = backups(lbx_backups.SelectedValue)
     End Sub
 
     Private Sub ckbx_startOnBoot_CheckedChanged(sender As Object, e As EventArgs) Handles ckbx_startOnBoot.CheckedChanged
         startOnBoot = ckbx_startOnBoot.Checked
         SaveOptions()
+    End Sub
+
+    Private Sub tmr_main_Tick(sender As Object, e As EventArgs) Handles tmr_main.Tick
+        For Each v In backups.Values
+            If v.MinutePast = Minute(Now) Then
+                v.CurrentInterval = v.CurrentInterval + 1
+            End If
+            If v.CurrentInterval = v.BackupInverval Then
+                QueueBackup(v.OriginalLoc, v.BackupLoc, v.PastVersions)
+                v.CurrentInterval = 0
+            End If
+        Next
+    End Sub
+
+    Private Sub btn_new_Click(sender As Object, e As EventArgs) Handles btn_new.Click
+        BackupAdder.Show()
+    End Sub
+
+    Public Sub QueueBackup(ByVal originalLoc As String, ByVal backupLoc As String, ByVal pastVersions As Integer)
+        queued.Add(queued.Count, New queueClass(originalLoc, backupLoc, pastVersions))
+    End Sub
+
+    Public Sub StartBackups()
+        For Each k In queued.Keys
+            Dim q As queueClass = queued(k)
+            secondThread.RunWorkerAsync(q)
+            queued.Remove(k)
+        Next
+        If queued.Count > 0 Then
+            StartBackups()
+        End If
+    End Sub
+
+    Private Sub secondThread_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles secondThread.DoWork
+        Try
+            Dim args As queueClass = e.Argument
+        Catch ex As Exception
+            Throw New ArgumentException("Argument couldn't be converted to queueClass")
+        End Try
     End Sub
 End Class
